7f5b57e5c4a286efe1ab98b03ddf2926dd7b0904
voiceinthedark <darkrisingforce@gmail.com>
2025-08-09 10:56:30 +0300

    hashset: Added unit testing to hashset

--- hashset.test.js
+++ hashset.test.js
@@ -2,9 +2,9 @@ import Hashset from "./hashset.js";
 
 describe('Hashset Tests', () => {
 
-  describe('Constructor tests...', ()=> {
+  describe('Constructor tests...', () => {
     const hashset = new Hashset();
-    
+
     test('Hashset has a capacity of 16', () => {
       expect(hashset.capacity).toBe(16)
     })
@@ -52,7 +52,89 @@ describe('Hashset Tests', () => {
       expect(hashset.capacity).toBe(32)
       expect(hashset.size).toBe(14)
     })
-
   })
-  
+
+  describe('Has method tests', () => {
+    let hashset;
+    beforeEach(() => {
+      hashset = new Hashset();
+      hashset.add('apple')
+      hashset.add('banana')
+      hashset.add('mango')
+    })
+    test('has method returns true if key exists', () => {
+      expect(hashset.has('apple')).toBeTruthy()
+      expect(hashset.has('grape')).toBeFalsy()
+    })
+  })
+
+  describe('Remove method tests', () => {
+    let hashset;
+    beforeEach(() => {
+      hashset = new Hashset();
+      hashset.add('apple')
+      hashset.add('banana')
+      hashset.add('mango')
+    })
+    test('remove a key from list decreases size', () => {
+      expect(hashset.size).toBe(3)
+      hashset.remove('mango')
+      expect(hashset.size).toBe(2)
+      expect(hashset.has('mango')).toBeFalsy()
+    });
+    test('remove an existant key returns true', () => {
+      expect(hashset.remove('apple')).toBeTruthy()
+    });
+    test('removing a non existant key returns false', () => {
+      expect(hashset.remove('water')).toBeFalsy()
+    });
+  });
+
+  describe('length method tests', () => {
+
+    let hashset;
+    beforeEach(() => {
+      hashset = new Hashset();
+      hashset.add('apple')
+      hashset.add('banana')
+      hashset.add('mango')
+    })
+
+    test('expect length of list to be 3', () => {
+      expect(hashset.length()).toBe(3)
+    })
+    test('an empty hashset returns 0 length', () => {
+      hashset = new Hashset();
+      expect(hashset.length()).toBe(0)
+    })
+  });
+
+  describe('clear method tests', () => {
+
+    let hashset;
+    beforeEach(() => {
+      hashset = new Hashset();
+      hashset.add('apple')
+      hashset.add('banana')
+      hashset.add('mango')
+    })
+    test('clear remove all keys from the list', () => {
+      hashset.clear()
+      expect(hashset.length()).toBe(0)
+      expect(hashset.has('apple')).toBeFalsy()
+    })
+  })
+  describe('keys method tests', () => {
+
+    let hashset;
+    beforeEach(() => {
+      hashset = new Hashset();
+      hashset.add('apple')
+      hashset.add('banana')
+      hashset.add('mango')
+    })
+    test('keys should return an array of correct key size', () => {
+      expect(hashset.keys()).toHaveLength(3)
+    })
+  })
 })
